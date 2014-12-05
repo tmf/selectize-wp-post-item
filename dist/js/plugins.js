3349dccb254d1a464abf1a5a96745dda922332b8
tmf <tom.forrer@gmail.com>
2014-12-05 14:41:04 +0100

    Released v0.0.2.

--- dist/js/plugins.js
+++ dist/js/plugins.js
@@ -42,7 +42,11 @@
 	
 	    options = $.extend({
 	        thumbnailField: 'post_thumbnail.thumbnail',
-	        thumbnailClassName: 'item-thumbnail'
+	        thumbnailClassName: 'item-thumbnail',
+	        titleField: 'post_title',
+	        dateField: 'post_date',
+	        excerptField: 'post_excerpt',
+	        authorField: 'post_author_nicename'
 	    }, options);
 	
 	    var self = this;
@@ -55,16 +59,34 @@
 	        return itemHtml;
 	    };
 	
+	    var formatPost = function(itemHtml, data){
+	        if(hasNestedProperty(data, options.titleField)){
+	            var $title = $('<span>').text(resolveNestedProperty(data, options.titleField)).addClass('title'),
+	                $date = $('<span>').text(resolveNestedProperty(data, options.dateField)).addClass('date'),
+	                $excerpt = $('<span>').text(resolveNestedProperty(data, options.excerptField)).addClass('excerpt'),
+	                $author = $('<span>').text(resolveNestedProperty(data, options.authorField)).addClass('author'),
+	                $itemHtml = $(itemHtml);
+	
+	            // remove original text matching the post title
+	            $itemHtml.contents().filter(function(){
+	                return this.nodeType == 3 && this.textContent.trim() === resolveNestedProperty(data, options.titleField);
+	            }).remove();
+	            $itemHtml.prepend([$title, $date, $author, $excerpt]);
+	            itemHtml = outerHtml($itemHtml);
+	        }
+	        return itemHtml;
+	    };
+	
 	    self.setup = (function () {
 	        var original = self.setup;
 	        return function () {
 	            var render_item = self.settings.render.item,
 	                render_option = self.settings.render.option;
 	            self.settings.render.item = function (data) {
-	                return prependThumbnail(render_item.apply(this, arguments), data);
+	                return prependThumbnail(formatPost(render_item.apply(this, arguments), data), data);
 	            };
 	            self.settings.render.option = function (data) {
-	                return prependThumbnail(render_option.apply(this, arguments), data);
+	                return prependThumbnail(formatPost(render_item.apply(this, arguments), data), data);
 	            };
 	            original.apply(self, arguments);
 	        };
